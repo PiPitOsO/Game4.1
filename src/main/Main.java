92627b3aee875827d66afc0ac7a3e0d2e1fbfcca
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2020-07-22 15:27:58 +0300

    finish1

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,83 +5,83 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder log = new StringBuilder();
 
         File main = new File("src//main");
         if (main.mkdir()) {
-            sb.append("Каталог main создан в scr\n");
+            log.append("Каталог main создан в scr\n");
         } else {
-            sb.append("ФКаталог main не создан\n");
+            log.append("ФКаталог main не создан\n");
         }
 
         File test = new File("scr//test");
         if (test.mkdir()) {
-            sb.append("Каталог test создан в scr\n");
+            log.append("Каталог test создан в scr\n");
     } else {
-            sb.append("Каталог test не создан\n");
+            log.append("Каталог test не создан\n");
     }
 
         File res = new File("res");
         if (res.mkdir()) {
-            sb.append("Каталог res создан\n");
+            log.append("Каталог res создан\n");
         } else {
-            sb.append("Каталог res не создан\n");
+            log.append("Каталог res не создан\n");
         }
 
         File drawables = new File("res//drawables");
         if (drawables.mkdir()) {
-            sb.append("Каталог drawables создан в res\n");
+            log.append("Каталог drawables создан в res\n");
         } else {
-            sb.append("Каталог drawables в res не создан\n");
+            log.append("Каталог drawables в res не создан\n");
         }
 
         File vectors = new File("res//vectors");
         if (vectors.mkdir()) {
-            sb.append("Каталог vectors создан в res\n");
+            log.append("Каталог vectors создан в res\n");
         } else {
-            sb.append("Каталог vectors в res не создан\n");
+            log.append("Каталог vectors в res не создан\n");
         }
 
         File icons = new File("res//icons");
         if (icons.mkdir()) {
-            sb.append("Каталог icons создан в res\n");
+            log.append("Каталог icons создан в res\n");
         } else {
-            sb.append("Каталог icons в res не создан\n");
+            log.append("Каталог icons в res не создан\n");
         }
 
         File savegames = new File("savegames");
         if (savegames.mkdir()) {
-            sb.append("Каталог savegames создан\n");
+            log.append("Каталог savegames создан\n");
         } else {
-            sb.append("Каталог savegames не создан\n");
+            log.append("Каталог savegames не создан\n");
         }
 
         File temp = new File("temp");
         if (temp.mkdir()) {
-            sb.append("Каталог temp создан\n");
+            log.append("Каталог temp создан\n");
         } else {
-            sb.append("Каталог temp не создан\n");
+            log.append("Каталог temp не создан\n");
         }
 
         File utils = new File("src//main//Utils.java");
         try {
             if (utils.createNewFile())
-                sb.append("Файл Utils.java создан\n");
+                log.append("Файл Utils.java создан\n");
         } catch (IOException ex) {
-            sb.append("Файл Utils.java не создан\n");
+            log.append("Файл Utils.java не создан\n");
             System.out.println(ex.getMessage());
         }
 
         File myFile = new File("temp//temp.txt");
         try {
             if (myFile.createNewFile())
-                sb.append("Файл temp.txt создан\n");
+                log.append("Файл temp.txt создан\n");
         } catch (IOException ex) {
-            sb.append("Файл temp.txt не создан\n");
+            log.append("Файл temp.txt не создан\n");
             System.out.println(ex.getMessage());
         }
 
-        String data = sb.toString();
+        String data = log.toString();
         FileWriter nFile = new FileWriter("temp//temp.txt");
         nFile.write(data);
         nFile.close();
